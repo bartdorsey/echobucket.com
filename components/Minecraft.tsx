3bdce34e2cd6ea21f72cecbf30a2dfbedd171e1f
Bart Dorsey <bart@bartdorsey.com>
2022-04-04 20:51:45 +0000

    Adding new useJavaTime hook

--- components/Minecraft.tsx
+++ components/Minecraft.tsx
@@ -1,10 +1,9 @@
 import styles from "../styles/Minecraft.module.css"
 import MinecraftStatus from "./MinecraftStatus"
 import MinecraftMap from './MinecraftMap';
-import { useInterval, useRequest } from 'ahooks';
-import axios from 'axios';
 import type { MinecraftStatusType } from "../lib/minecraft";
 import type { Properties, Regions } from "../lib/Unmined";
+import useJavaTime from '../hooks/useJavaTime';
 
 type MinecraftProps = {
   java_status: MinecraftStatusType,
@@ -13,28 +12,13 @@ type MinecraftProps = {
   regions: Regions
 }
 
-type Response = {
-  time: string
-}
-
 export default function Minecraft({
   java_status,
   status,
   properties,
   regions,
 }: MinecraftProps) {
-  const { data, run } = useRequest<Response, []>(async () => {
-    const { data } = await axios("/api/minecraft/javatime");
-    return data;
-  }, {
-    manual: true
-  });
-
-  useInterval(async () => {
-    run();
-  }, 2000, {
-    immediate: true
-  })
+  const time = useJavaTime();
 
   return (
     <div className={styles.container}>
@@ -42,7 +26,7 @@ export default function Minecraft({
       <div className={styles.minecraft}>
         <div className={styles.status}>
           <MinecraftStatus status={status} />
-          <MinecraftStatus status={{ ...java_status, time: data?.time }} />
+          <MinecraftStatus status={{ ...java_status, time }} />
         </div>
         <MinecraftMap properties={properties} regions={regions}/>
       </div>
