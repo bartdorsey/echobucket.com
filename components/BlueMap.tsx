add3ed4dd2cca2323f685bc5a77e2bbbaf102aa9
Bart Dorsey <bart@bartdorsey.com>
2022-04-04 21:16:27 +0000

    Adding env var for bluemap

--- components/BlueMap.tsx
+++ components/BlueMap.tsx
@@ -1,8 +1,9 @@
 import { useRef } from 'react';
 import styles from '../styles/Minecraft.module.css';
+const BLUEMAP_URL = process.env.BLUEMAP_URL ?? 'http://javamcmap.echobucket.com'
 export default function BlueMap() {
   const mapRef = useRef();
   return (
-    <iframe ref={mapRef.current} className={styles.map} src="http://javamcmap.echobucket.com"/>
+    <iframe ref={mapRef.current} className={styles.map} src={BLUEMAP_URL} />
   )
 }
