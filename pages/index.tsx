04cf21f5717a3516967fcfcf4c44d097aec14940
Bart Dorsey <bart@bartdorsey.com>
2022-04-20 15:05:56 +0000

    Adding home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,48 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Link from "next/link"
+import styles from "../styles/Home.module.css"
+import { SocialIcon } from "react-social-icons"
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>echobucket.com</title>
+        <title>echobucket</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          echobucket.com
-        </h1>
+        <h1 className={styles.title}>echobucket</h1>
+        <p className={styles.description}>
+          I'm a developer and video game player. You can follow my socials below
+        </p>
+        <ul className={styles.socials}>
+          <li>
+            <SocialIcon
+              className={styles.socialIcon}
+              fgColor="#ffffff"
+              url="https://twitch.tv/echobucket"
+            />
+          </li>
+          <li>
+            <SocialIcon
+              className={styles.socialIcon}
+              fgColor="#ffffff"
+              url="https://youtube.com/echobucket"
+            />
+          </li>
+          <li>
+            <SocialIcon
+              className={styles.socialIcon}
+              fgColor="#ffffff"
+              url="https://instagram.com/_echobucket"
+            />
+          </li>
+        </ul>
       </main>
-
       <footer className={styles.footer}>
+        <Link href="/minecraft">Minecraft Server Status</Link>
       </footer>
     </div>
   )
