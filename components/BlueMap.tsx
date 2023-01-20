f2351213d3a3a11f3cfb99ebd3e814a972c0e4c6
Bart Dorsey <bart@bartdorsey.com>
2023-01-20 15:16:53 -0600

    Astro version

--- components/BlueMap.tsx
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-import styles from '../styles/Minecraft.module.css';
-const BLUEMAP_URL = process.env.NEXT_PUBLIC_BLUEMAP_URL ?? 'http://javamcmap.echobucket.com'
-export default function BlueMap() {
-  const mapRef = useRef();
-  return (
-    <iframe ref={mapRef.current} className={styles.map} src={BLUEMAP_URL} />
-  )
-}
