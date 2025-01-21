e9ac45d608ad1fde0a28526009624b854d2441d0
Bart Dorsey <bart@bartdorsey.com>
2025-01-21 13:36:00 -0600

    Updating to next 14

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputStandalone: true
-  },
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/minecraft/tiles/:path*',
-        destination: 'http://mc.echobucket.com/minecraft/tiles/:path*'
-      }
-    ]
-  }
+    output: 'standalone',
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/minecraft/tiles/:path*',
+                destination: 'http://mc.echobucket.com/minecraft/tiles/:path*',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
